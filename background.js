c8303e0bc5f4da4ac2d1eed4995e07cd6f00b6bd
BJ Bryant <bbryant@axisaccounting.com>
2017-05-17 20:07:33 -0500

    Updated tab creation so it opens next to active tab.

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
+// Received from content.js
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     switch (msg.type) {
         case "ns-json-new":
-            console.log(msg);
             storeAndDisplayJson(msg);
             break;
     }
     return true;
 });
 
+// Received from popup.js
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     switch (msg.type) {
         case "ns-json-get":
@@ -19,18 +20,23 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     return true;
 });
 
+// Injects click.js into Netsuite page for NS API context
 chrome.browserAction.onClicked.addListener (function (tab) {
     chrome.tabs.executeScript(null, {
         file: 'click.js'
     });
 });
 
+// Saves the json and creates a new tab
 var storeAndDisplayJson = function (msg) {
     chrome.storage.local.set({
         json: msg.data
     });
 
-    chrome.tabs.create({url: chrome.extension.getURL('popup.html')});
+    chrome.tabs.query({active: true}, function (tabs) {
+        var index = tabs[0].index;
+        chrome.tabs.create({url: chrome.extension.getURL('popup.html'), index: index + 1});
+    });
 }
 
 
